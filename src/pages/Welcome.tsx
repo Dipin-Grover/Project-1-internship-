cd931ebd024d0fa6b9e2ea70b2a3a2904aa817bc
Dipin-Grover <groverdipin003@gmail.com>
2025-09-02 18:53:17 +0530

    designed home page

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,17 +1,20 @@
-import { useNavigate } from "react-router-dom";
+// Welcome.tsx
+import React from 'react';
+import '../index.css';
 
-const Welcome = () => {
-  const navigate = useNavigate();
-
+const Welcome: React.FC = () => {
   return (
-    <div className="main-wrapper">
-      <h1>Welcome Page</h1>
-      <button className="btn" onClick={() => navigate("/login")}>
-        Login
-      </button>
-      <button className="btn" onClick={() => navigate("/register")}>
-        Create Account
-      </button>
+    <div className="welcome-container">
+      <div className="welcome-card">
+        <div className="card-content">
+          <h1>Welcome to PopX</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <div className="card-actions">
+          <button className="create-btn">Create Account</button>
+          <button className="login-btn">Already Registered? Login</button>
+        </div>
+      </div>
     </div>
   );
 };
