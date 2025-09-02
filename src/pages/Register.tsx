83739cea46d3c119f9066899be740fced5313c56
Dipin-Grover <groverdipin003@gmail.com>
2025-09-02 21:50:58 +0530

    make some fields compulsory in creating acccount

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,43 +1,66 @@
 import React from 'react';
 
-const Register: React.FC = () => {
-  return (
-    <div className="welcome-container">
-      <div className="welcome-card">
-        <form>
-          <h1>Create your<br />PopX account</h1>
-          <div className="form-group">
-            <label>Full Name</label>
-            <input type="text" placeholder="Enter full name" />
+const Register: React.FC = () => (
+  <div className="welcome-container">
+    <div className="welcome-card">
+      <form>
+        <h1>Create your<br />PopX account</h1>
+
+        <div className="form-group">
+          <label>
+            Full Name <span className="required-star">*</span>
+          </label>
+          <input type="text" placeholder="Enter full name" required />
+        </div>
+
+        <div className="form-group">
+          <label>
+            Phone Number <span className="required-star">*</span>
+          </label>
+          <input type="text" placeholder="Enter phone number" required />
+        </div>
+
+        <div className="form-group">
+          <label>
+            Email Address <span className="required-star">*</span>
+          </label>
+          <input type="email" placeholder="Enter email address" required />
+        </div>
+
+        <div className="form-group">
+          <label>
+            Password <span className="required-star">*</span>
+          </label>
+          <input type="password" placeholder="Enter password" required />
+        </div>
+
+        <div className="form-group">
+          <label>
+            Company Name
+          </label>
+          <input type="text" placeholder="Enter company name" />
+        </div>
+
+        <div className="form-group">
+          <label>
+            Are you an Agency? <span className="required-star">*</span>
+          </label>
+          <div className="radio-group">
+            <label>
+              <input type="radio" name="agency" value="yes" required /> Yes
+            </label>
+            <label>
+              <input type="radio" name="agency" value="no" required /> No
+            </label>
           </div>
-          <div className="form-group">
-            <label>Phone Number</label>
-            <input type="text" placeholder="Enter phone number" />
-          </div>
-          <div className="form-group">
-            <label>Email Address</label>
-            <input type="email" placeholder="Enter email address" />
-          </div>
-          <div className="form-group">
-            <label>Password</label>
-            <input type="password" placeholder="Enter password" />
-          </div>
-          <div className="form-group">
-            <label>Company Name</label>
-            <input type="text" placeholder="Enter company name" />
-          </div>
-          <div className="form-group">
-            <label>Are you an Agency?</label>
-            <div>
-              <label><input type="radio" name="agency" value="yes" /> Yes</label>
-              <label><input type="radio" name="agency" value="no" /> No</label>
-            </div>
-          </div>
-          <button className="create-btn" type="submit">Create Account</button>
-        </form>
-      </div>
+        </div>
+
+        <button className="create-btn" type="submit">
+          Create Account
+        </button>
+      </form>
     </div>
-  );
-};
+  </div>
+);
 
 export default Register;
