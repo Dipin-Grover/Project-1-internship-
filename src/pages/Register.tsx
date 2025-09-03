b1d2783d6ce8f36d742b2e91a583d8d092fe7aa3
Dipin-Grover <groverdipin003@gmail.com>
2025-09-03 18:58:52 +0530

    fixed 404 error

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,87 +1,73 @@
-
-
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const Register: React.FC = () => (
-  <div className="welcome-container">
-    <div className="welcome-card">
-      <form>
-        <h1>Create your<br />PopX account</h1>
+const Register: React.FC = () => {
+  const navigate = useNavigate();
 
-        <fieldset className="form-fieldset">
-          <legend className="form-legend">
-            Full Name <span className="required-star">*</span>
-          </legend>
-          <input type="text" placeholder="Enter full name" required />
-        </fieldset>
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    // Optionally, validate form here
+    navigate('/account'); // Redirect to profile page
+  }
 
-        <fieldset className="form-fieldset">
-          <legend className="form-legend">
-            Phone Number <span className="required-star">*</span>
-          </legend>
-          <input type="text" placeholder="Enter phone number" required />
-        </fieldset>
+  return (
+    <div className="welcome-container">
+      <div className="welcome-card">
+        <form onSubmit={handleSubmit}>
+          <h1>Create your<br />PopX account</h1>
 
-        <fieldset className="form-fieldset">
-          <legend className="form-legend">
-            Email Address <span className="required-star">*</span>
-          </legend>
-          <input type="email" placeholder="Enter email address" required />
-        </fieldset>
+          <fieldset className="form-fieldset">
+            <legend className="form-legend">
+              Full Name <span className="required-star">*</span>
+            </legend>
+            <input type="text" placeholder="Enter full name" required />
+          </fieldset>
 
-        <fieldset className="form-fieldset">
-          <legend className="form-legend">
-            Password <span className="required-star">*</span>
-          </legend>
-          <input type="password" placeholder="Enter password" required />
-        </fieldset>
+          <fieldset className="form-fieldset">
+            <legend className="form-legend">
+              Phone Number <span className="required-star">*</span>
+            </legend>
+            <input type="text" placeholder="Enter phone number" required />
+          </fieldset>
 
-        <fieldset className="form-fieldset">
-          <legend className="form-legend">Company Name</legend>
-          <input type="text" placeholder="Enter company name" />
-        </fieldset>
+          <fieldset className="form-fieldset">
+            <legend className="form-legend">
+              Email Address <span className="required-star">*</span>
+            </legend>
+            <input type="email" placeholder="Enter email address" required />
+          </fieldset>
 
-        <div className="agency-inline">
-          <label className="agency-label">
-            Are you an Agency? <span className="required-star">*</span>
-          </label>
-          <label>
-            <input type="radio" name="agency" value="yes" required /> Yes
-          </label>
-          <label>
-            <input type="radio" name="agency" value="no" required /> No
-          </label>
-        </div>
+          <fieldset className="form-fieldset">
+            <legend className="form-legend">
+              Password <span className="required-star">*</span>
+            </legend>
+            <input type="password" placeholder="Enter password" required />
+          </fieldset>
 
-        <button className="create-btn" type="submit">
-          Create Account
-        </button>
-      </form>
+          <fieldset className="form-fieldset">
+            <legend className="form-legend">Company Name</legend>
+            <input type="text" placeholder="Enter company name" />
+          </fieldset>
+
+          <div className="agency-inline">
+            <label className="agency-label">
+              Are you an Agency? <span className="required-star">*</span>
+            </label>
+            <label>
+              <input type="radio" name="agency" value="yes" required /> Yes
+            </label>
+            <label>
+              <input type="radio" name="agency" value="no" required /> No
+            </label>
+          </div>
+
+          <button className="create-btn" type="submit">
+            Create Account
+          </button>
+        </form>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Register;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
