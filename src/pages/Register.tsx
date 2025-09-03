736665273f227d75fc318d84135a7bbc5a9b84f8
Dipin-Grover <groverdipin003@gmail.com>
2025-09-03 18:23:45 +0530

    improved UI

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,3 +1,5 @@
+
+
 import React from 'react';
 
 const Register: React.FC = () => (
@@ -6,53 +8,49 @@ const Register: React.FC = () => (
       <form>
         <h1>Create your<br />PopX account</h1>
 
-        <div className="form-group">
-          <label>
+        <fieldset className="form-fieldset">
+          <legend className="form-legend">
             Full Name <span className="required-star">*</span>
-          </label>
+          </legend>
           <input type="text" placeholder="Enter full name" required />
-        </div>
+        </fieldset>
 
-        <div className="form-group">
-          <label>
+        <fieldset className="form-fieldset">
+          <legend className="form-legend">
             Phone Number <span className="required-star">*</span>
-          </label>
+          </legend>
           <input type="text" placeholder="Enter phone number" required />
-        </div>
+        </fieldset>
 
-        <div className="form-group">
-          <label>
+        <fieldset className="form-fieldset">
+          <legend className="form-legend">
             Email Address <span className="required-star">*</span>
-          </label>
+          </legend>
           <input type="email" placeholder="Enter email address" required />
-        </div>
+        </fieldset>
 
-        <div className="form-group">
-          <label>
+        <fieldset className="form-fieldset">
+          <legend className="form-legend">
             Password <span className="required-star">*</span>
-          </label>
+          </legend>
           <input type="password" placeholder="Enter password" required />
-        </div>
+        </fieldset>
 
-        <div className="form-group">
-          <label>
-            Company Name
-          </label>
+        <fieldset className="form-fieldset">
+          <legend className="form-legend">Company Name</legend>
           <input type="text" placeholder="Enter company name" />
-        </div>
+        </fieldset>
 
-        <div className="form-group">
-          <label>
+        <div className="agency-inline">
+          <label className="agency-label">
             Are you an Agency? <span className="required-star">*</span>
           </label>
-          <div className="radio-group">
-            <label>
-              <input type="radio" name="agency" value="yes" required /> Yes
-            </label>
-            <label>
-              <input type="radio" name="agency" value="no" required /> No
-            </label>
-          </div>
+          <label>
+            <input type="radio" name="agency" value="yes" required /> Yes
+          </label>
+          <label>
+            <input type="radio" name="agency" value="no" required /> No
+          </label>
         </div>
 
         <button className="create-btn" type="submit">
@@ -64,3 +62,26 @@ const Register: React.FC = () => (
 );
 
 export default Register;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
